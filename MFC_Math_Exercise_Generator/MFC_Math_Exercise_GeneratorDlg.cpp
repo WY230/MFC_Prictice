f07dff8a8d07d96d3c0a998bf3136d98f323c9ab
WY230 <219697588+WY230@users.noreply.github.com>
2025-07-21 11:30:45 +0800

    实现 MFC_Math_Exercise_Generator的随机出题功能

--- MFC_Math_Exercise_Generator/MFC_Math_Exercise_GeneratorDlg.cpp
+++ MFC_Math_Exercise_Generator/MFC_Math_Exercise_GeneratorDlg.cpp
@@ -7,6 +7,8 @@
 #include "MFC_Math_Exercise_Generator.h"
 #include "MFC_Math_Exercise_GeneratorDlg.h"
 #include "afxdialogex.h"
+#include <ctime>  // 包含 time() 函数
+
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -14,7 +16,24 @@
 
 
 // 用于应用程序“关于”菜单项的 CAboutDlg 对话框
-
+const int CMFC_Math_Exercise_GeneratorDlg::IDC_QS_ARRAY[20] = {
+	IDC_QS1, IDC_QS2, IDC_QS3, IDC_QS4, IDC_QS5,
+	IDC_QS6, IDC_QS7, IDC_QS8, IDC_QS9, IDC_QS10,
+	IDC_QS11, IDC_QS12, IDC_QS13, IDC_QS14, IDC_QS15,
+	IDC_QS16, IDC_QS17, IDC_QS18, IDC_QS19, IDC_QS20
+};
+const int CMFC_Math_Exercise_GeneratorDlg::IDC_AS_ARRAY[20] = {
+	IDC_AS1, IDC_AS2, IDC_AS3, IDC_AS4, IDC_AS5,
+	IDC_AS6, IDC_AS7, IDC_AS8, IDC_AS9, IDC_AS10,
+	IDC_AS11, IDC_AS12, IDC_AS13, IDC_AS14, IDC_AS15,
+	IDC_AS16, IDC_AS17, IDC_AS18, IDC_AS19, IDC_AS20
+};
+const int CMFC_Math_Exercise_GeneratorDlg::IDC_TF_ARRAY[20] = {
+	IDC_TF1, IDC_TF2, IDC_TF3, IDC_TF4, IDC_TF5,
+	IDC_TF6, IDC_TF7, IDC_TF8, IDC_TF9, IDC_TF10,
+	IDC_TF11, IDC_TF12, IDC_TF13, IDC_TF14, IDC_TF15,
+	IDC_TF16, IDC_TF17, IDC_TF18, IDC_TF19, IDC_TF20
+};
 class CAboutDlg : public CDialogEx
 {
 public:
@@ -51,68 +70,7 @@ END_MESSAGE_MAP()
 
 
 CMFC_Math_Exercise_GeneratorDlg::CMFC_Math_Exercise_GeneratorDlg(CWnd* pParent /*=nullptr*/)
-	: CDialogEx(IDD_MY6__DIALOG, pParent)
-	, m_qs1(_T(""))
-	, m_qs2(_T(""))
-	, m_qs3(_T(""))
-	, m_qs4(_T(""))
-	, m_qs5(_T(""))
-	, m_qs6(_T(""))
-	, m_qs7(_T(""))
-	, m_qs8(_T(""))
-	, m_qs9(_T(""))
-	, m_qs10(_T(""))
-	, m_qs12(_T(""))
-	, m_qs11(_T(""))
-	, m_qs13(_T(""))
-	, m_qs14(_T(""))
-	, m_qs15(_T(""))
-	, m_qs16(_T(""))
-	, m_qs17(_T(""))
-	, m_qs18(_T(""))
-	, m_qs19(_T(""))
-	, m_qs20(_T(""))
-	, m_as1(_T(""))
-	, m_as2(_T(""))
-	, m_as3(_T(""))
-	, m_as4(_T(""))
-	, m_as5(_T(""))
-	, m_as6(_T(""))
-	, m_as7(_T(""))
-	, m_as8(_T(""))
-	, m_as9(_T(""))
-	, m_as10(_T(""))
-	, m_as11(_T(""))
-	, m_as12(_T(""))
-	, m_as13(_T(""))
-	, m_as14(_T(""))
-	, m_as15(_T(""))
-	, m_as16(_T(""))
-	, m_as17(_T(""))
-	, m_as18(_T(""))
-	, m_as19(_T(""))
-	, m_as20(_T(""))
-	, m_tf1(_T(""))
-	, m_tf2(_T(""))
-	, m_tf3(_T(""))
-	, m_tf4(_T(""))
-	, m_tf5(_T(""))
-	, m_tf6(_T(""))
-	, m_tf7(_T(""))
-	, m_tf8(_T(""))
-	, m_tf9(_T(""))
-	, m_tf10(_T(""))
-	, m_tf11(_T(""))
-	, m_tf12(_T(""))
-	, m_tf13(_T(""))
-	, m_tf14(_T(""))
-	, m_tf15(_T(""))
-	, m_tf16(_T(""))
-	, m_tf17(_T(""))
-	, m_tf18(_T(""))
-	, m_tf19(_T(""))
-	, m_tf20(_T(""))
-	, m_score(_T(""))
+	: CDialogEx(IDD_MY6__DIALOG, pParent),m_score(_T(""))
 {
 	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
 }
@@ -120,66 +78,20 @@ CMFC_Math_Exercise_GeneratorDlg::CMFC_Math_Exercise_GeneratorDlg(CWnd* pParent /
 void CMFC_Math_Exercise_GeneratorDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
-	DDX_Text(pDX, IDC_QS1, m_qs1);
-	DDX_Text(pDX, IDC_QS2, m_qs2);
-	DDX_Text(pDX, IDC_QS3, m_qs3);
-	DDX_Text(pDX, IDC_QS4, m_qs4);
-	DDX_Text(pDX, IDC_QS5, m_qs5);
-	DDX_Text(pDX, IDC_QS6, m_qs6);
-	DDX_Text(pDX, IDC_QS7, m_qs7);
-	DDX_Text(pDX, IDC_QS8, m_qs8);
-	DDX_Text(pDX, IDC_QS9, m_qs9);
-	DDX_Text(pDX, IDC_QS10, m_qs10);
-	DDX_Text(pDX, IDC_QS12, m_qs12);
-	DDX_Text(pDX, IDC_QS11, m_qs11);
-	DDX_Text(pDX, IDC_QS13, m_qs13);
-	DDX_Text(pDX, IDC_QS14, m_qs14);
-	DDX_Text(pDX, IDC_QS15, m_qs15);
-	DDX_Text(pDX, IDC_QS16, m_qs16);
-	DDX_Text(pDX, IDC_QS17, m_qs17);
-	DDX_Text(pDX, IDC_QS18, m_qs18);
-	DDX_Text(pDX, IDC_QS19, m_qs19);
-	DDX_Text(pDX, IDC_QS20, m_qs20);
-	DDX_Text(pDX, IDC_AS1, m_as1);
-	DDX_Text(pDX, IDC_AS2, m_as2);
-	DDX_Text(pDX, IDC_AS3, m_as3);
-	DDX_Text(pDX, IDC_AS4, m_as4);
-	DDX_Text(pDX, IDC_AS5, m_as5);
-	DDX_Text(pDX, IDC_AS6, m_as6);
-	DDX_Text(pDX, IDC_AS7, m_as7);
-	DDX_Text(pDX, IDC_AS8, m_as8);
-	DDX_Text(pDX, IDC_AS9, m_as9);
-	DDX_Text(pDX, IDC_AS10, m_as10);
-	DDX_Text(pDX, IDC_AS11, m_as11);
-	DDX_Text(pDX, IDC_AS12, m_as12);
-	DDX_Text(pDX, IDC_AS13, m_as13);
-	DDX_Text(pDX, IDC_AS14, m_as14);
-	DDX_Text(pDX, IDC_AS15, m_as15);
-	DDX_Text(pDX, IDC_AS16, m_as16);
-	DDX_Text(pDX, IDC_AS17, m_as17);
-	DDX_Text(pDX, IDC_AS18, m_as18);
-	DDX_Text(pDX, IDC_AS19, m_as19);
-	DDX_Text(pDX, IDC_AS20, m_as20);
-	DDX_Text(pDX, IDC_TF1, m_tf1);
-	DDX_Text(pDX, IDC_TF2, m_tf2);
-	DDX_Text(pDX, IDC_TF3, m_tf3);
-	DDX_Text(pDX, IDC_TF4, m_tf4);
-	DDX_Text(pDX, IDC_TF5, m_tf5);
-	DDX_Text(pDX, IDC_TF6, m_tf6);
-	DDX_Text(pDX, IDC_TF7, m_tf7);
-	DDX_Text(pDX, IDC_TF8, m_tf8);
-	DDX_Text(pDX, IDC_TF9, m_tf9);
-	DDX_Text(pDX, IDC_TF10, m_tf10);
-	DDX_Text(pDX, IDC_TF11, m_tf11);
-	DDX_Text(pDX, IDC_TF12, m_tf12);
-	DDX_Text(pDX, IDC_TF13, m_tf13);
-	DDX_Text(pDX, IDC_TF14, m_tf14);
-	DDX_Text(pDX, IDC_TF15, m_tf15);
-	DDX_Text(pDX, IDC_TF16, m_tf16);
-	DDX_Text(pDX, IDC_TF17, m_tf17);
-	DDX_Text(pDX, IDC_TF18, m_tf18);
-	DDX_Text(pDX, IDC_TF19, m_tf19);
-	DDX_Text(pDX, IDC_TF20, m_tf20);
+	for (int i = 0; i < 20; i++)
+	{
+		DDX_Text(pDX, IDC_QS_ARRAY[i], m_qs[i]);
+	}
+
+	for (int i = 0; i < 20; i++)
+	{
+		DDX_Text(pDX, IDC_AS_ARRAY[i], m_as[i]);
+	}
+
+	for (int i = 0; i < 20; i++)
+	{
+		DDX_Text(pDX, IDC_TF_ARRAY[i], m_tf[i]);
+	}
 	DDX_Text(pDX, IDC_SCORE, m_score);
 }
 
@@ -222,7 +134,8 @@ BOOL CMFC_Math_Exercise_GeneratorDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// 设置小图标
 
 	// TODO: 在此添加额外的初始化代码
-
+	srand(time(NULL));
+	Createmyqs();
 	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
 }
 
@@ -274,4 +187,49 @@ HCURSOR CMFC_Math_Exercise_GeneratorDlg::OnQueryDragIcon()
 {
 	return static_cast<HCURSOR>(m_hIcon);
 }
-
+//随机生成2和3作为每道题目的运算次数
+int CMFC_Math_Exercise_GeneratorDlg::Createtime()
+{
+	
+	return 2 + rand() % 2;
+}
+//生成随机运算符
+void CMFC_Math_Exercise_GeneratorDlg::Createsymbol(CString& m_str)
+{
+	char symbol[] = { '+','-','*','/' };
+		m_str = symbol[0 + rand() % 4];
+}
+//生成随机数字
+void CMFC_Math_Exercise_GeneratorDlg::Createnum(CString& m_str)
+{
+	
+		int num=0+ rand() % 100;
+		 m_str.Format(_T("%d"), num);
+	
+}
+//生成算题
+void CMFC_Math_Exercise_GeneratorDlg::Createmyqs()
+{
+	
+	for (int n=0; n < 20; n++)
+	{
+		int i = Createtime();
+		for (int j = 0; j < i; j++)
+		{
+			if (j == 0)
+			{
+				Createnum(m_strnum);
+				m_qs[n] = m_strnum;
+			}
+			else
+			{
+				Createsymbol(m_strsymbol);
+				m_qs[n] += m_strsymbol;
+				Createnum(m_strnum);
+				m_qs[n] += m_strnum;
+			}
+			
+		}
+	}
+	UpdateData(FALSE);
+}
